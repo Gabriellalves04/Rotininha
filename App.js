5dbc7549ff79acf4a82560835dbdcc6cf5878df2
vicLira <victor.liracarlos@gmail.com>
2024-04-16 14:25:43 -0300

    adição do flatlist

--- App.js
+++ App.js
@@ -1,54 +1,31 @@
 import React from "react";
-import { Text, StyleSheet, View, Image } from "react-native";
+import { Text, StyleSheet, View, Image, FlatList} from "react-native";
 import Header from "./components/header";
 import Card from "./components/card";
+import Rotina from './data';
+
 export default function App() {
   return (
     <View style={estilo.container}>
       <Header></Header>
-      <Card
-        porcentagem="5.95%"
-        titulo="Transporte Ida e Volta para a escola"
-        imagem='5.png'
-        descricao="Segunda a Sexta"
-      ></Card>
-      <Card
-        porcentagem="41.67%"
-        titulo="Dormir"
-        imagem="2.png"
-        descricao="Todos os dias"
-      ></Card>
-      <Card
-        porcentagem="15.48%"
-        titulo="Etec"
-        imagem="3.png"
-        descricao="Segunda a Sexta"
-      ></Card>
-      <Card
-        porcentagem="8.33%"
-        titulo="Almoço e Janta"
-        imagem="1.png"
-        descricao="Todos os dias"
-      ></Card>
-      <Card
-        porcentagem="11.67%"
-        titulo="Arrumar e Organizar a casa"
-        imagem="6.png"
-        descricao="Todos os dias"
-      ></Card>
-      <Card
-        porcentagem="16,9"
-        titulo="Igreja"
-        imagem="9.png"
-        descricao="Todos os dias"
-      ></Card>
+    <FlatList
+    data = {Rotina}
+    keyExtractor = {(item)=>item.id}
+    renderItem = {({item}) =>(
+       <Card
+       porcentagem = {item.porcentagem}
+       titulo = {item.titulo}
+       imagem = {item.imagem}
+       descricao = {item.descricao}
+       />
+    )} 
+    />
     </View>
   );
 }
 const estilo = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     backgroundColor: "#CAF0F8",
     borderColor: "#90E0EF",
     paddingHorizontal: 20,
